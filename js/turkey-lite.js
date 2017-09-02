fa9794dfbe79c07b5b199d1d8eb436aebce8d82f
Brandon Dang <frogninja@sbcglobal.net>
2017-09-01 21:15:26 -0500

    adding 'specfic' auditors

--- js/turkey-lite.js
+++ js/turkey-lite.js
@@ -1,12 +1,18 @@
 var auditors = {
   "before_typing_delay": true,
+  "clicks_specific": true,
   "clicks_total": true,
+  "focus_changes": true,
   "focus_changes_total": true,
+  "keypresses_specific": true,
   "keypresses_total": true,
+  "mouse_movement_specific": true,
   "mouse_movement_total": true,
   "on_focus_time": true,
+  "pastes_specific": true,
   "pastes_total": true,
   "recorded_time_disparity": true,
+  "scrolled_pixels_specific": true,
   "scrolled_pixels_total": true,
   "total_task_time": true,
   "within_typing_delay": true,
@@ -19,6 +25,6 @@ for (var name in auditors) {
     $("<script />")
       .attr("type", "text/javascript")
       .attr("src", "js/auditors/" + name + ".js")
-      .appendTo("head")
+      .appendTo("head");
   }
 }
